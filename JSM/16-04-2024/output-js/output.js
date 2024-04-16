135098885eeacaff52597e126e01b67a2cac09ae
Farhan Nawwafal Pramudia <142996272+Farhan-Nawwafal@users.noreply.github.com>
2024-04-16 21:21:09 +0700

    Add files via upload

---
+++ JSM/16-04-2024/output-js/output.js
@@ -0,0 +1,11 @@
+/*
+Output js :
+1. console.log() --> tampilkan ke konsol.
+2. document.write() --> tampilkan ke halaman/body html
+*/
+
+// document.write()
+document.write("Hello World");
+
+// console.log()
+console.log("Hello World!");
---
+++ JSM/16-04-2024/variables-js/variables.js
@@ -0,0 +1,27 @@
+/* 
+format pembuatan variable di js :
+keywordVar namaVariabel = value;
+
+keywordVar :
+1. var
+2. let --> lebih disarankan daripada var.
+3. const --> konstanta
+*/
+
+// let
+let nama = "lily";
+console.log(nama);
+document.write(nama);
+nama = "sakura"; // mengubah nilai
+
+//const
+const bunga = "tulip";
+console.log(bunga);
+// bunga = "lily"; // ctrl + /
+
+// operator concatenation (+)
+let namaAwal = "Hello";
+let namaAkhir = "World";
+console.log(namaAwal + " " + namaAkhir); // Hello World
+
+console.log("test");
